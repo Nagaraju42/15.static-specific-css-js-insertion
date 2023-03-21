cc9a82082b7c47d318502c0e9018791f82ce70b5
Nagaraju42 <nagarajukalngi@gmail.com>
2023-03-21 16:38:01 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Gamora(request):
+    return render(request,'Gamora.html')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Thanos(request):
+    return render(request,'Thanos.html')
